409374d7b361c46be00b9a72679891c7b458af97
marcelomiky <marcelo.mine@gmail.com>
2017-05-30 13:36:03 -0300

    new folder, Cousera

---
+++ elefante.py
@@ -0,0 +1,11 @@
+def elefante(x):
+
+	'''Função que canta a música clássica do elefante'''
+
+	texto1 = "elefante incomoda muita gente"
+	texto2 = "incomoda"
+	texto3 = "muito mais"
+
+	for i in range(x + 1):
+		print("%d", %(i, texto1))
+
