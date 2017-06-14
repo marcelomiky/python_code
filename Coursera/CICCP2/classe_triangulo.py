1932d1392c1be272c8d06723c6c77a0d6a754320
marcelomiky <marcelo.mine@gmail.com>
2017-06-14 15:58:19 -0300

    POO exercises

---
+++ Coursera/CICCP2/classe_triangulo.py
@@ -0,0 +1,9 @@
+class Triangulo:
+    
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+        
+    def perimetro(self):
+        return self.a + self.b + self.c
---
+++ Coursera/CICCP2/tipo_triangulo.py
@@ -0,0 +1,14 @@
+class Triangulo:
+    
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+        
+    def tipo_lado(self):
+        if self.a == self.b and self.a == self.c:
+            return 'equilátero'
+        elif self.a != self.b and self.a != self.c and self.b != self.c:
+            return 'escaleno'
+        else:
+            return 'isósceles'
