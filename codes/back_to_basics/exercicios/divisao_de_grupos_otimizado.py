83be7d0a5d735eb9845b1e32bb5bff03c715c175
marcelomiky <marcelo.mine@gmail.com>
2018-01-26 15:21:27 -0200

    some updates

--- codes/back_to_basics/exercicios/divisao_de_grupos_otimizado.py
+++ codes/back_to_basics/exercicios/divisao_de_grupos_otimizado.py
@@ -1,6 +1,6 @@
 def divide_grupos(alunos, grupos):
 	'''
-	Função que recebe a quantidade de alunos e grupos e faz a divisão dos grupos, ainda não é de forma otimizada.
+	Função que recebe a quantidade de alunos e grupos e faz a divisão dos grupos, agora sim de forma otimizada.
 	
 	>>> divide_grupos(40, 5)
 	8
@@ -12,22 +12,24 @@ def divide_grupos(alunos, grupos):
 	[3, 5, 1, 6]
 
 	>>> divide_grupos(38, 8)
-	[7, 4, 1, 10]
+	[6, 5, 2, 4]
 	'''
 
 	if alunos % grupos == 0:
 		return alunos // grupos
 	else:
-		lista = []
-		al1 = alunos // grupos
-		gr1 = grupos - 1
-		gr2 = grupos - gr1
-		al2 = alunos - gr1 * al1
-		lista.append(gr1)
-		lista.append(al1)	
-		lista.append(gr2)
-		lista.append(al2)
-		return lista
+		if (alunos % grupos < alunos // grupos):
+			lista = []
+			al1 = alunos // grupos
+			gr1 = grupos - 1
+			gr2 = grupos - gr1
+			al2 = alunos - gr1 * al1
+			lista.append(gr1)
+			lista.append(al1)	
+			lista.append(gr2)
+			lista.append(al2)
+			return lista
+		#else:
 
 	if __name__ == '__main__':
 		import doctest
