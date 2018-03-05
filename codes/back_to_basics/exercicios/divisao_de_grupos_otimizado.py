ff60b4e30d99c2872712498f3671faa4d731bbd8
marcelomiky <marcelo.mine@gmail.com>
2018-03-05 15:36:49 -0300

    now it is done, finito, fin

--- codes/back_to_basics/exercicios/divisao_de_grupos_otimizado.py
+++ codes/back_to_basics/exercicios/divisao_de_grupos_otimizado.py
@@ -1,23 +1,21 @@
 def divide_grupos(alunos, grupos):
-	'''
-	Função que recebe a quantidade de alunos e grupos e faz a divisão dos grupos, agora sim de forma otimizada. 
+    """Function that gets the total of students and groups and does the division, in a optimized way.
+    If it's a round division, returns the number of students per group
+    If it's not a round division, the list presents:
+    [quantity of groups of type 1 quantity of students per groups of type 1, quantity of groups of type 1 quantity of students per groups of type 2]
 
-    Se for uma divisão exata, retorna a quantidade de alunos por grupo. 
+    >>> divide_grupos(40, 5)
+    8
 
-    Se não for uma divisão exata, a lista apresenta: [quantidade de grupos do tipo 1, quantidade de alunos por grupo do tipo 1, quantidade de grupos do tipo 2, quantidade de alunos por grupo do tipo 2] 
-	
-	>>> divide_grupos(40, 5)
-	8
-	
-	>>> divide_grupos(20, 4)
-	5
+    >>> divide_grupos(20, 4)
+    5
 
-	>>> divide_grupos(21, 4)
-	[3, 5, 1, 6]
+    >>> divide_grupos(21, 4)
+    [3, 5, 1, 6]
 
-	>>> divide_grupos(38, 8)
-	[7, 5, 1, 3]
-	'''
+    >>> divide_grupos(38, 8)
+    [6, 5, 2, 4]
+    """
     
     if alunos % grupos == 0:
         return alunos // grupos
@@ -39,13 +37,24 @@ def divide_grupos(alunos, grupos):
         gr1 = grupos - 1
         gr2 = grupos - gr1
         al2 = alunos - gr1 * al1
-        lista.append(gr1)
-        lista.append(al1)	
-        lista.append(gr2)
-        lista.append(al2)
-        return lista
+        if al1 - al2 >= 2:
+            lista_nova = []
+            gr1 -= 1
+            gr2 += 1
+            al2 += 1
+            lista_nova.append(gr1)
+            lista_nova.append(al1)	
+            lista_nova.append(gr2)
+            lista_nova.append(al2)
+            return lista_nova
+        else:
+            lista.append(gr1)
+            lista.append(al1)	
+            lista.append(gr2)
+            lista.append(al2)
+            return lista
             
 
-	if __name__ == '__main__':
-		import doctest
-		doctest.testmod()	
+    if __name__ == '__main__':
+        import doctest
+        doctest.testmod()
