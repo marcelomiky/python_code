1adaea5d623787dc1610c461ee1512b88f5e159f
marcelomiky <marcelomiky@sc.usp.br>
2018-07-30 13:52:54 -0300

    first commit

---
+++ codes/back_to_basics/conta_palavra.py
@@ -0,0 +1,19 @@
+from string import split
+
+
+def count_words(sentence):
+    '''
+    Funcao que conta a quantidade de palavras numa string
+    '''
+    string = sentence
+    counter = 0
+    count_blank_spaces = string.count(' ')
+
+    for i in count_blank_spaces:
+        if 
+    return count_blank_spaces
+
+print(count_words("frase")) == 0
+print(count_words("frase aaa")) == 1
+#print(count_words("frase")) == 5
+#print(count_words("frase maior")) == 11
