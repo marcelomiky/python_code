8859ad1f79c2ef5acac0026aefb0deb00753dc5c
marcelomiky <marcelomiky@sc.usp.br>
2018-07-30 13:55:30 -0300

    first commit, test file

---
+++ codes/back_to_basics/test_conta_palavra.py
@@ -0,0 +1,5 @@
+print(count_words("frase")) == 1
+print(count_words("frase aaa")) == 2
+print(count_words("frase1 aaa outrafrase")) == 3
+print(count_words("frase1 aaa outrafrase    ")) == 3
+print(count_words("frase1 aaa outrafrase    xyz")) == 4
