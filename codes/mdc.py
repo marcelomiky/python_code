fa261cb8c80b529b1428af3ed8d82e1d08dbf6c0
marcelomiky <marcelo.mine@gmail.com>
2019-06-04 09:37:35 -0300

    criadas algumas funções, arrumados alguns bugs

--- codes/mdc.py
+++ codes/mdc.py
@@ -16,24 +16,67 @@ def eh_primo(x):
     else:
         return 1
 
+
 #print(eh_primo(3))
 #print(eh_primo(2))
 #print(eh_primo(17))
 
 
-def cria_lista_numeros_primos_ate_n_sobre_2(n):
+def cria_lista_numeros_primos_ate_n(n):
     """
-    Função que retorna todos os números primos de 1 até n/2
+    Função que retorna todos os números primos de 1 até n
     """
     lista_primos = list()
-    for i in range(1, n//2):
+    for i in range(1, n):
         if eh_primo(i) == 1:
             lista_primos.append(i)
 
     return lista_primos
 
-#print(cria_lista_numeros_primos_ate_n_sobre_2(30))  # [1, 2, 3, 5, 7, 11, 13]
-#print(cria_lista_numeros_primos_ate_n_sobre_2(20))  # [1, 2, 3, 5, 7]
+
+# print(cria_lista_numeros_primos_ate_n(30))  # [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+# print(cria_lista_numeros_primos_ate_n(20))  # [1, 2, 3, 5, 7, 11, 13, 17, 19]
+
+
+def lista_divisores_do_mdc(x):
+    lista_primos = cria_lista_numeros_primos_ate_n(x)
+    divisor = lista_primos[1]
+    counter = 0
+    lista_divisores = [lista_primos[0]]
+    temp_x = x
+
+    while temp_x != 1:
+        if temp_x % divisor == 0:
+            lista_divisores.append(divisor)
+            temp_x /= divisor
+        else:
+            counter += 1
+            divisor = lista_primos[counter]
+
+    return lista_divisores
+
+
+# x = 1024
+# print(lista_divisores_do_mdc(x))
+
+
+def lista_divisores(x):
+
+    lista_para_dividir = list(range(1, (x)//2 + 1))
+    lista_divisores = list()
+    temp_x = x
+
+    for i in lista_para_dividir:
+        if temp_x % i == 0:
+            lista_divisores.append(i)
+
+    lista_divisores.append(x)
+
+    return lista_divisores
+
+
+# x = 100
+# print("lista_divisores(x)", lista_divisores(x))
 
 
 def intersection(lst1, lst2):  # From here: https://www.geeksforgeeks.org/python-intersection-two-lists/
@@ -47,6 +90,20 @@ def intersection(lst1, lst2):  # From here: https://www.geeksforgeeks.org/python
         return lst3_temp2
 
 
+def numeros_primos_entre_si(a, b):
+    if eh_primo(a) == 1 and eh_primo(b) == 1:
+        return 1
+    elif len(intersection(lista_divisores(a), lista_divisores(b))) == 1:
+        return 1
+    else:
+        return 0
+
+
+# print(cria_lista_numeros_primos_ate_n(3))
+# print(cria_lista_numeros_primos_ate_n(4))
+# print("numeros_primos_entre_si(3, 4):", numeros_primos_entre_si(3, 4))
+
+
 def multiply(numbers):  # From here: https://www.w3resource.com/python-exercises/python-functions-exercise-3.php
     total = 1
     for x in numbers:
@@ -55,60 +112,73 @@ def multiply(numbers):  # From here: https://www.w3resource.com/python-exercises
 
 
 def mdc(var1, var2):
-    lista_primos_var1 = cria_lista_numeros_primos_ate_n_sobre_2(var1)
 
-    divisor = lista_primos_var1[1]
-    # print("Divisor[0]:", divisor)
-    counter = 0
-    lista_divisores1 = list()
-    temp_var1 = var1
-    
-    while temp_var1 != 1:
-        if temp_var1 % divisor == 0:
-            lista_divisores1.append(divisor)
-            temp_var1 /= divisor
-        else:
-            counter += 1
-            divisor = lista_primos_var1[counter]
+    if numeros_primos_entre_si(var1, var2):  # para quando faz mdc entre números primos entre eles, como 3 e 4
+        return 1
+    else:
+        lista_divisores1 = lista_divisores_do_mdc(var1)
+        lista_divisores2 = lista_divisores_do_mdc(var2)
 
-    lista_primos_var2 = cria_lista_numeros_primos_ate_n_sobre_2(var2)
+        # lista_primos_var1 = cria_lista_numeros_primos_ate_n(var1)
+        #
+        # print("lista_primos_var1:", lista_primos_var1)
+        #
+        # divisor = lista_primos_var1[1]
+        # # print("Divisor[0]:", divisor)
+        # counter = 0
+        # lista_divisores1 = list()
+        # temp_var1 = var1
+        #
+        # while temp_var1 != 1:
+        #     if temp_var1 % divisor == 0:
+        #         lista_divisores1.append(divisor)
+        #         temp_var1 /= divisor
+        #     else:
+        #         counter += 1
+        #         divisor = lista_primos_var1[counter]
+        #
+        # lista_primos_var2 = cria_lista_numeros_primos_ate_n(var2)
+        #
+        # print("lista_primos_var2:", lista_primos_var2)
+        #
+        # divisor = lista_primos_var2[1]
+        # counter = 0
+        # lista_divisores2 = list()
+        # temp_var2 = var2
+        #
+        # while temp_var2 != 1:
+        #     if temp_var2 % divisor == 0:
+        #         lista_divisores2.append(divisor)
+        #         temp_var2 /= divisor
+        #     else:
+        #         counter += 1
+        #         divisor = lista_primos_var2[counter]
 
-    divisor = lista_primos_var2[1]
-    counter = 0
-    lista_divisores2 = list()
-    temp_var2 = var2
+        print("LISTA DIVISORES 1: ", lista_divisores1)
+        print("LISTA DIVISORES 2: ", lista_divisores2)
 
-    while temp_var2 != 1:
-        if temp_var2 % divisor == 0:
-            lista_divisores2.append(divisor)
-            temp_var2 /= divisor
-        else:
-            counter += 1
-            divisor = lista_primos_var2[counter]
 
-    print("LISTA DIVISORES 1: ", lista_divisores1)
-    print("LISTA DIVISORES 2: ", lista_divisores2)
+        lista_interseccao_divisores = intersection(lista_divisores1, lista_divisores2)
+        # lista_interseccao_divisores = set(lista_divisores1).intersection(set(lista_divisores2))
 
-    lista_interseccao_divisores = intersection(lista_divisores1, lista_divisores2)
-    # lista_interseccao_divisores = set(lista_divisores1).intersection(set(lista_divisores2))
+        print("lista_interseccao_divisores:", lista_interseccao_divisores)
 
-    print("lista_interseccao_divisores:", lista_interseccao_divisores)
-
-    return multiply(lista_interseccao_divisores)
+        return multiply(lista_interseccao_divisores)
 
 
 # a = 12
 # b = 8
 # print("MDC({},{}) = {}".format(a, b, mdc(a, b)))  # = 4
-#
+
 # a = 12
 # b = 24
 # print("MDC({},{}) = {}".format(a, b, mdc(a, b)))  # = 12
-#
+
+# CASO DE ERRO! ARRUMAR NO insersection()
 # a = 120
 # b = 150
 # print("MDC({},{}) = {}".format(a, b, mdc(a, b)))  # = 30
-#
+
 # a = 180
 # b = 36
 # print("MDC({},{}) = {}".format(a, b, mdc(a, b)))  # = 36
@@ -117,9 +187,18 @@ def mdc(var1, var2):
 # b = 252
 # print("MDC({},{}) = {}".format(a, b, mdc(a, b)))  # = 36
 
-a = 180
-b = 108
-print("MDC({},{}) = {}".format(a, b, mdc(a, b)))  # = 36
+# a = 180
+# b = 108
+# print("MDC({},{}) = {}".format(a, b, mdc(a, b)))  # = 36
+
+# a = 3
+# b = 4
+# print("MDC({},{}) = {}".format(a, b, mdc(a, b)))  # = 1
+
+# a = 10
+# b = 11
+# print("MDC({},{}) = {}".format(a, b, mdc(a, b)))  # = 1
+
 
 
 """
